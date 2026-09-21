aea61f609af3cb2df31b3efac0de2d1d2d1e7d63
xoinj24 <studytkyik@gmail.com>
2026-09-21 08:42:27 +0000

    создание меню

---
+++ index.ts
@@ -0,0 +1,13 @@
+let n1: number = 0;
+let n2: number =0;
+
+function displayMenu(): void {
+    console.log("МЕНЮ");
+    console.log("1. Ввести два числа");
+    console.log("2. Выполнить сложение");
+    console.log("3. Выполнить вычитание");
+    console.log("4. Выполнить деление");
+    console.log("5. Возвести число в степень");
+}
+
+displayMenu();
